eb4a6264e809857f4c8bf11804fd0b788577ca13
Nam Nguyen <nam.nguyenh@lgcns.com>
2025-03-10 16:55:11 +0700

    add cluster exist check function

--- manual/app/gke/scripts/create_cluster.sh
+++ manual/app/gke/scripts/create_cluster.sh
@@ -5,32 +5,47 @@ CLUSTER_NAME="cluster1"
 REGION="us-west1"
 MAX_RETRIES=60  # 30 minutes timeout with 30 seconds interval
 RETRY_INTERVAL=30
-
+# Check if cluster existing
+check_cluster_exist() {
+  local status=$(gcloud container clusters describe $CLUSTER_NAME --region $REGION >/dev/null 2>&1)
+  echo "Cluster status: $status"
+  if [ "$?" == "0" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
 # Create GKE cluster
 echo "Creating GKE cluster '$CLUSTER_NAME'..."
-gcloud container clusters create $CLUSTER_NAME \
-    --region $REGION \
-    --network demo \
-    --num-nodes 2 \
-    --machine-type e2-small \
-    --enable-autoscaling \
-    --min-nodes 1 \
-    --max-nodes 3 \
-    --disk-size 50 \
-    --enable-ip-alias \
-    --enable-network-policy \
-    --cluster-ipv4-cidr 10.0.0.0/14 \
-    --logging="SYSTEM,API_SERVER,WORKLOAD" \
-    --monitoring="SYSTEM,API_SERVER,POD,DEPLOYMENT,STATEFULSET,STORAGE" \
-    --maintenance-window-start 2025-03-11T04:00:00Z \
-    --maintenance-window-end 2025-03-11T10:00:00Z \
-    --maintenance-window-recurrence 'FREQ=WEEKLY;BYDAY=MO,TU,FR'
 
-# Check if the cluster creation was successful
-if [ $? -ne 0 ]; then
-  echo "Failed to create GKE cluster '$CLUSTER_NAME'. Exiting."
-  exit 1
-fi
+
+  if check_cluster_exist; then
+    echo "Cluster '$CLUSTER_NAME' is existing."
+    exit 0
+  else
+    gcloud container clusters create $CLUSTER_NAME \
+        --region $REGION \
+        --network demo \
+        --num-nodes 2 \
+        --machine-type e2-small \
+        --enable-autoscaling \
+        --min-nodes 1 \
+        --max-nodes 3 \
+        --disk-size 50 \
+        --enable-ip-alias \
+        --enable-network-policy \
+        --cluster-ipv4-cidr 10.0.0.0/14 \
+        --logging="SYSTEM,API_SERVER,WORKLOAD" \
+        --monitoring="SYSTEM,API_SERVER,POD,DEPLOYMENT,STATEFULSET,STORAGE" \
+        --maintenance-window-start 2025-03-11T04:00:00Z \
+        --maintenance-window-end 2025-03-11T10:00:00Z \
+        --maintenance-window-recurrence 'FREQ=WEEKLY;BYDAY=MO,TU,FR'
+    # Check if the cluster creation was successful
+    if [ $? -ne 0 ]; then
+        echo "Failed to create GKE cluster '$CLUSTER_NAME'. Exiting."
+        exit 1
+    fi
+  fi
 
 # Function to check cluster status
 check_cluster_status() {
