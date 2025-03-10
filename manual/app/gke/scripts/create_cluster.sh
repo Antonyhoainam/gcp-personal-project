4ae08cde6d52540970e79fb1995db68aeaa63ab2
Nam Nguyen <nam.nguyenh@lgcns.com>
2025-03-10 15:48:10 +0700

    init project with gke setup, argocd installation and applicatin deployment with cloudbuild

---
+++ manual/app/gke/scripts/create_cluster.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Variables
+CLUSTER_NAME="cluster1"
+REGION="us-west1"
+MAX_RETRIES=60  # 30 minutes timeout with 30 seconds interval
+RETRY_INTERVAL=30
+
+# Create GKE cluster
+echo "Creating GKE cluster '$CLUSTER_NAME'..."
+gcloud container clusters create $CLUSTER_NAME \
+    --region $REGION \
+    --num-nodes 3 \
+    --machine-type e2-small \
+    --enable-autoscaling \
+    --min-nodes 1 \
+    --max-nodes 5 \
+    --enable-ip-alias \
+    --enable-network-policy \
+    --cluster-ipv4-cidr 10.0.0.0/14 \
+    --enable-cloud-logging \
+    --enable-cloud-monitoring \
+    --maintenance-window "tue:00:00-tue:06:00" \
+    --maintenance-window "wed:00:00-wed:06:00" \
+    --maintenance-window "fri:00:00-fri:06:00"
+
+# Check if the cluster creation was successful
+if [ $? -ne 0 ]; then
+  echo "Failed to create GKE cluster '$CLUSTER_NAME'. Exiting."
+  exit 1
+fi
+
+# Function to check cluster status
+check_cluster_status() {
+  local status=$(gcloud container clusters describe $CLUSTER_NAME --region $REGION --format="value(status)")
+  echo "Cluster status: $status"
+  if [ "$status" == "RUNNING" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+# Main script
+echo "Checking if GKE cluster '$CLUSTER_NAME' is ready..."
+
+for ((i=1; i<=MAX_RETRIES; i++)); do
+  if check_cluster_status; then
+    echo "Cluster '$CLUSTER_NAME' is ready."
+    exit 0
+  else
+    echo "Cluster '$CLUSTER_NAME' is not ready yet. Retrying in $RETRY_INTERVAL seconds... ($i/$MAX_RETRIES)"
+    sleep $RETRY_INTERVAL
+  fi
+done
+
+echo "Cluster '$CLUSTER_NAME' is not ready after 30 minutes timeout."
+exit 1
---
+++ manual/app/gke/scripts/install_argocd.sh
@@ -0,0 +1,10 @@
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+# Expose argocd API
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+
+kubectl port-forward svc/argocd-server -n argocd 8080:443
+
+# Install argocd cli
+curl -sSL -o argocd https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+chmod +x argocd
+sudo mv argocd /usr/local/bin/
